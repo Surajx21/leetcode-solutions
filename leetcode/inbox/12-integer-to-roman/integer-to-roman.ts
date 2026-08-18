7c57679bb34a1a0fcb8fc09fed38418752b8f0e1
Suraj Shah <94737162+Surajx21@users.noreply.github.com>
2026-08-18 16:30:17 +0530

    Time: 10 ms (27.45%) | Memory: 64.9 MB (15.38%) - LeetSync

---
+++ leetcode/inbox/12-integer-to-roman/integer-to-roman.ts
@@ -0,0 +1,29 @@
+function intToRoman(num: number): string {
+  let romanNumber = "";
+
+  const map = new Map<number, string>([
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ]);
+
+  for (const [key, roman] of map) {
+    const rem = Math.floor(num / key);
+
+    romanNumber += roman.repeat(rem);
+
+    num %= key;
+  }
+
+  return romanNumber;
+}
