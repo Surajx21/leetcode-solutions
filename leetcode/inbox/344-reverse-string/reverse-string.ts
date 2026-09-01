b1017ba7ca8c679dccfd772e64156880e7c96cbf
Suraj Shah <94737162+Surajx21@users.noreply.github.com>
2026-09-01 22:33:40 +0530

    Time: 0 ms (100.00%) | Memory: 62 MB (64.76%) - LeetSync

---
+++ leetcode/inbox/344-reverse-string/reverse-string.ts
@@ -0,0 +1,16 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    let i = 0
+    let j = s.length - 1
+
+    while (i < j){
+        const temp = s[j];
+        s[j] = s[i];
+        s[i] = temp
+
+        i++
+        j--
+    }
+};
