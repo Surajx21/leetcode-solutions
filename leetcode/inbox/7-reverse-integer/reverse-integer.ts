4b3d585057b6c17b5f61e38c07b71461e6e143ba
Suraj Shah <94737162+Surajx21@users.noreply.github.com>
2026-09-04 23:23:28 +0530

    Time: 57 ms (35.54%) | Memory: 57.2 MB (78.69%) - LeetSync

---
+++ leetcode/inbox/7-reverse-integer/reverse-integer.ts
@@ -0,0 +1,21 @@
+function reverse(x: number): number {
+    let sign = x >= 0 ? 1 : -1
+    let result = 0
+    let limit = Math.pow(2, 31) - 1
+    x = Math.abs(x)
+    let i = 0;
+
+    while (x != 0 && i < 10) {
+        let remainder = x % 10;
+        result = (result * 10) 
+
+        if (result >= limit){
+            return 0
+        }
+        
+        result += remainder
+        x = Math.floor(x / 10)
+        i++
+    }
+    return result * sign
+};
